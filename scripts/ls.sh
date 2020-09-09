09abf4c006af3a3203a480c83c9ed5c772e7d009
root <azaa1>
2020-09-09 22:53:34 +0000

    scripts folder added

---
+++ scripts/ls.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# setting variable "a" to "ls" command
+a="pwd"
+# calling variable "a" with "echo" command
+echo $a
+# call variable "a"
+$a  
---
+++ scripts/simple2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+echo -e "Hello World 1 \n \nHello World 2 \n \nHello World 3"
