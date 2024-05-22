15a0152d4eef09edf6e0d5c4c2703fafb7ef647c
azaa1 <azaa.chi11@gmail.com>
2024-05-22 11:41:01 -0500

    added export_variables.sh script

---
+++ scripts/export_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script sets the following environment variables
+# Run the script: source export_vaiables.sh 
+
+# Script to export environment variables
+export Key1="Value1"
+export Key2="Value2"
